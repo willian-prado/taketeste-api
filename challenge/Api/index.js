287759d6fa4c89a82290117b690f04fec206a95f
Willian Prado <willian_prado@hotmail.com>
2022-05-21 20:55:46 -0300

    feat(Api): add customRouter to express app

--- challenge/Api/index.js
+++ challenge/Api/index.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const App = require('./App');
+const CustomRouter = require('./routes/CustomRouter');
+const Service = require('./services/Service');
+const Controller = require('./controllers/Controller');
 
 const app = new App();
+const controller = new Controller(Service);
+const customRouter = new CustomRouter(controller);
 
+app.addRouter(customRouter.router);
 app.startServer(process.env.PORT || 3000);
